52c1a75d9da19836391be99e119c1e889966173a
Goran1001 <goranskendzic@gmail.com>
2023-03-08 02:00:21 +0100

    Commit message

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+import message.MyMessage;
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, everybody!");
+        MyMessage.printMessage();
+    }
+}
